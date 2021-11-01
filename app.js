794dd5671022229f6779ad752e0e70392f318bbc
dikiogres <dikiogres@gmail.com>
2021-11-01 21:11:26 +0700

    deploy challenge 5

--- app.js
+++ app.js
@@ -2,10 +2,37 @@ const express = require('express');
 const app = express(); 
 const morgan = require('morgan');
 const router = require('./router');
+const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use(morgan('dev'));
 
-app.listen(3000);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+}
+
+app.use(logger);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 app.use(router);
+
+app.use(function(err, req, res, next) {
+    console.error(err)
+    res.status(500).json({
+        status: 'fail',
+        errors: err.message
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        errors: 'Page not found'
+    })
+    next();
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- router.js
+++ router.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const user = require('./posts.json')
+
+let posts = require('./posts.json');
+
+router.use((req, res, next) => {
+    console.log('Time: ', new Date(Date.now()));
+    next()
+})
 
 router.use(express.static('public'));
 
@@ -11,8 +19,29 @@ router.get('/suit-game', (req,res)=>{
     res.render('suit-game');
 })
 
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    let userEmail = user.find(x => x.email === email);
+    let userPassword = user.find(x => x.password === password);
+    if(!userEmail){
+        res.json("Email not found!");
+    }
+    else if(userEmail && !userPassword){
+        res.json("Wrong Password!");
+    }
+    else{
+        res.json(userEmail);
+    }
+
+})
+
 router.use((req,res)=>{
     res.status(404).render('404');
 })
+
 
 module.exports = router;
