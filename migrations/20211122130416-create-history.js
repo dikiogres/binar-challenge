b54a926dc1052770f0c7e118ecd69d2abca3f88f
dikiogres <85748069+dikiogres@users.noreply.github.com>
2021-11-24 23:56:00 +0700

    ch6

---
+++ migrations/20211122130416-create-history.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      player_choice: {
+        type: Sequelize.TEXT
+      },
+      com_choice: {
+        type: Sequelize.TEXT
+      },
+      player_result: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Histories');
+  }
+};
